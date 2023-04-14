f9f1d7d7110b25c12d651ac63b7dfd218e6a28fc
Anik Das <onikd08@gmail.com>
2023-04-14 13:30:17 +0300

    Add attribution link in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,9 @@ const Header = () => {
   return (
     <div>
       <h3>Header</h3>
+      <a href="https://www.flaticon.com/free-icons/person" title="person icons">
+        Person icons created by Freepik - Flaticon
+      </a>
     </div>
   );
 };
